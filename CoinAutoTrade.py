da84c74bd79dc1e6f8e028795bd9166a6dba9713
HyojunK <gywns0608@naver.com>
2022-01-11 23:44:06 +0900

    add class autoTrade

--- CoinAutoTrade.py
+++ CoinAutoTrade.py
@@ -2,37 +2,48 @@ import pyupbit
 import time
 import datetime
 
-# 장 시작시간과 종료시간은 00:00:00 ~ 11:59:59로 설정
-
-# 목표가 계산
-def get_target_price(ticker, k):
-    data = pyupbit.get_ohlcv(ticker)
-
-    # 전일 가격 데이터
-    yesterday = data.iloc[-2]
-    # 전일 종가(= 금일 시가)
-    today_open = yesterday['close']
-    # 변동폭 = 전일 고가 - 전일 저가
-    range = yesterday['high'] - yesterday['low']
-    # 목표가
-    target = today_open + range * k
-
-    return target
-
-# 현재 시간
-now = datetime.datetime.now()
-# 시작 시간
-openTime = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-# 목표가
-targetPrice = get_target_price("KRW-BTC", 0.5)
-
-while True:
-    now = datetime.datetime.now()
-    if openTime < now < openTime + datetime.timedelta(seconds=5) :
-        targetPrice = get_target_price("KRW-BTC", 0.5)
-        openTime = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+class autoTrade :
+    def __init__(self, start_cash) :
+        self.fee = 0.05 # 수수료
+        self.target_price = 0 # 목표 매수가
+        self.bull = False # 상승장 여부
         
-    currentPrice = pyupbit.get_current_price("KRW-BTC")
-    print(currentPrice)
+        self.start_cash = start_cash # 시작 자산
+        self.current_cash = start_cash # 현재 자산
+        self.highest_cash = start_cash # 자산 최고점
+        self.lowest_cash = start_cash # 자산 최저점
 
-    time.sleep(1)
+        self.ror = 1 # 수익률
+
+        self.trade_count = 0 # 거래횟수
+        self.win_count = 0 # 승리횟수
+
+    def start(self) :
+        while True :
+            current_price = pyupbit.get_current_price("KRW-BTC")
+            print(current_price)
+            time.sleep(1)
+
+    def get_today_data(self) :
+        daily_data = pyupbit.get_ohlcv("KRW-BTC", count=41)
+        # 노이즈 계산 ( 1- 절대값(시가 - 종가) / (고가 - 저가) )
+        daily_data['noise'] = 1 - abs(daily_data['open'] - daily_data['close']) / (daily_data['high'] - daily_data['low'])
+        # 노이즈 20일 평균
+        daily_data['noise_ma20'] = daily_data['noise'].rolling(window=20).mean().shift(1)
+       
+        # 변동폭 ( 고가 - 저가 )
+        daily_data['range'] = daily_data['high'] - daily_data['low']
+        # 목표매수가 ( 시가 + 변동폭 * K )
+        daily_data['targetPrice'] = daily_data['open'] + daily_data['range'].shift(1) * daily_data['noise_ma20']
+
+        # 5일 이동평균선
+        daily_data['ma5'] = daily_data['close'].rolling(window=5, min_periods=1).mean().shift(1)
+        # 상승장 여부
+        daily_data['bull'] = daily_data['open'] > daily_data['ma5']
+
+        today = daily_data.iloc[-1]
+
+        return today
+
+tradingBot = autoTrade(1000000)
+today_data = tradingBot.start()
